aabd370e9dfeb74dfd880568c21ebf5d4872d7b4
Yuzo <hirakawa.yuzo@gmail.com>
2020-10-08 18:46:21 +0900

    予定登録機能の実装

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -3,4 +3,25 @@ class PlansController < ApplicationController
     @plans = Plan.all
     @plan = Plan.new
   end
+
+  def create
+    binding.pry
+    @plan = Plan.new(plan_params)
+    binding.pry
+    if @plan.save
+      binding.pry
+      redirect_to root_path
+    else
+      binding.pry
+      @plans = Plan.all
+      render :index
+    end
+  end
+
+  private
+
+  def plan_params
+    params.permit( :title, :start_time, :ending_time, :date_pattern, :comment, user_ids: []).merge(master_id: current_user.id)
+  end
+
 end
