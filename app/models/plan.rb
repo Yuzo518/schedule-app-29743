868564f0ad3e5c9287cdd11c1c04b9ad74b59c40
Yuzo <hirakawa.yuzo@gmail.com>
2020-10-08 18:53:20 +0900

    バリデーションの設定

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,4 +2,11 @@ class Plan < ApplicationRecord
   # アソシエーション
   has_many :plan_users
   has_many :users, through: :plan_users
+
+   # バリデーション
+   validates :title, presence: true
+   validates :date_pattern, presence: true
+   validates :start_time, presence: true
+   validates :ending_time, presence: true
+   validates :master_id, presence: true
 end
