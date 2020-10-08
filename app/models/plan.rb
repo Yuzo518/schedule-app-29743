c5809b1fbfa48efa502f202ed2e88e13b37123fe
Yuzo <hirakawa.yuzo@gmail.com>
2020-10-08 21:02:11 +0900

    単体テスト

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -4,9 +4,10 @@ class Plan < ApplicationRecord
   has_many :users, through: :plan_users
 
   # バリデーション
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 20 }
   validates :date_pattern, presence: true
   validates :start_time, presence: true
   validates :ending_time, presence: true
   validates :master_id, presence: true
+  validates :comment, length: { maximum: 500 }
 end
--- spec/factories/plans.rb
+++ spec/factories/plans.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
   factory :plan do
+    title         { 'TestTitle' }
+    date_pattern  { '通常' }
+    start_time    { Time.now }
+    ending_time   { Time.now }
+    comment       { Faker::Team.name }
+    master_id     { 2 }
   end
 end
