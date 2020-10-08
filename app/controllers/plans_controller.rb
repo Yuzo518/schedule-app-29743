3d4b11eaab050ae2691d88d0e2d0a2880d1ff0d1
Yuzo <hirakawa.yuzo@gmail.com>
2020-10-08 19:04:28 +0900

    rubocopによるコード修正

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -5,14 +5,10 @@ class PlansController < ApplicationController
   end
 
   def create
-    binding.pry
     @plan = Plan.new(plan_params)
-    binding.pry
     if @plan.save
-      binding.pry
       redirect_to root_path
     else
-      binding.pry
       @plans = Plan.all
       render :index
     end
@@ -21,7 +17,6 @@ class PlansController < ApplicationController
   private
 
   def plan_params
-    params.permit( :title, :start_time, :ending_time, :date_pattern, :comment, user_ids: []).merge(master_id: current_user.id)
+    params.permit(:title, :start_time, :ending_time, :date_pattern, :comment, user_ids: []).merge(master_id: current_user.id)
   end
-
 end
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -3,10 +3,10 @@ class Plan < ApplicationRecord
   has_many :plan_users
   has_many :users, through: :plan_users
 
-   # バリデーション
-   validates :title, presence: true
-   validates :date_pattern, presence: true
-   validates :start_time, presence: true
-   validates :ending_time, presence: true
-   validates :master_id, presence: true
+  # バリデーション
+  validates :title, presence: true
+  validates :date_pattern, presence: true
+  validates :start_time, presence: true
+  validates :ending_time, presence: true
+  validates :master_id, presence: true
 end
