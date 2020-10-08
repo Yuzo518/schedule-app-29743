f67a547f810a191082c7580d2bf20a055cf02fe1
Yuzo <hirakawa.yuzo@gmail.com>
2020-10-08 15:27:27 +0900

    plansテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_054711) do
+ActiveRecord::Schema.define(version: 2020_10_08_061922) do
+
+  create_table "plans", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "date_pattern", null: false
+    t.datetime "start_time", null: false
+    t.datetime "ending_time", null: false
+    t.text "comment"
+    t.integer "master_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
