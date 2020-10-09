297315de9f7f5cab2774a968dc7ab585028e6df9
Yuzo <hirakawa.yuzo@gmail.com>
2020-10-09 23:12:08 +0900

    予定変更機能実装

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -5,10 +5,14 @@ class PlansController < ApplicationController
   end
 
   def create
+    binding.pry
     @plan = Plan.new(plan_params)
+    binding.pry
     if @plan.save
+      binding.pry
       redirect_to root_path
     else
+      binding.pry
       @plans = Plan.all
       render :index
     end
@@ -19,9 +23,35 @@ class PlansController < ApplicationController
     @plans = Plan.all
   end
 
+  def edit
+    @plan = Plan.find(params[:id])
+    @plans = Plan.all
+    # 選択されていないselectを表示させる回数を決める
+    @users_count = 6 - @plan.users.count
+  end
+
+  def update
+    binding.pry
+    @plan = Plan.find(params[:id])
+    binding.pry
+    if @plan.update(edit_plan_params)
+      binding.pry
+      redirect_to root_path
+    else
+      @plans = Plan.all
+      # 選択されていないselectを表示させる回数を決める
+      @users_count = 6 - @plan.users.count
+      render 'edit'
+    end
+  end
+
   private
 
   def plan_params
     params.permit(:title, :start_time, :ending_time, :date_pattern, :comment, user_ids: []).merge(master_id: current_user.id)
+  end
+
+  def edit_plan_params
+    params.require(:plan).permit(:title, :start_time, :ending_time, :date_pattern, :comment, user_ids: []).merge(master_id: current_user.id)
   end
 end
