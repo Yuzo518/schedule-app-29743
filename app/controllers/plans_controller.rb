710e94ac8ebbdc71379fa342cf2490ccd7927527
Yuzo <hirakawa.yuzo@gmail.com>
2020-10-11 10:18:48 +0900

    viewページ調整

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -5,14 +5,10 @@ class PlansController < ApplicationController
   end
 
   def create
-    binding.pry
     @plan = Plan.new(plan_params)
-    binding.pry
     if @plan.save
-      binding.pry
       redirect_to root_path
     else
-      binding.pry
       @plans = Plan.all
       render :index
     end
@@ -31,11 +27,8 @@ class PlansController < ApplicationController
   end
 
   def update
-    binding.pry
     @plan = Plan.find(params[:id])
-    binding.pry
     if @plan.update(edit_plan_params)
-      binding.pry
       redirect_to root_path
     else
       @plans = Plan.all
